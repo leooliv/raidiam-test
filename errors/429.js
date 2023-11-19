dc7dc4b9de2ba590c7752aa0424d15fe42c435bd
leooliv <leonardo.dolivier@gmail.com>
2023-11-19 20:23:41 -0300

    Added comments

--- errors/429.js
+++ errors/429.js
@@ -1,1 +1,3 @@
-//
+/** To simulate this error I would need to send multiple requests
+ * to the API with something like a for loop.
+ **/
--- errors/5xx.js
+++ errors/5xx.js
@@ -1,1 +1,5 @@
-// 
+/**
+ * To simulate internal server errors I would need to know the defects
+ * of the API, we could try, but since for this test we're using a robust
+ * API it would be hard and improbable to get a code 5xx
+ */
