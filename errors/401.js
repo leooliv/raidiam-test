a549faf58dad2307bf45abd232295c34f7083b02
leooliv <leonardo.dolivier@gmail.com>
2023-11-19 20:18:07 -0300

    Added README

--- errors/401.js
+++ errors/401.js
@@ -1,12 +1,12 @@
-const API_URL = `${process.env.BASE_URL}?lat=10,10&lon=10,10`;
+const API_URL = `${process.env.BASE_URL}?lat=10,10&lon=10,10`
 
 const triggerError401 = () => {
   return new Promise((resolve) => {
     fetch(API_URL).then((res) => {
-      console.log("401 RESPONSE:", res.status);
-      resolve();
-    });
-  });
-};
+      console.log('401 RESPONSE:', res.status)
+      resolve()
+    })
+  })
+}
 
-module.exports = triggerError401;
+module.exports = triggerError401
